c29d1ab827d88bbb7d7e0aa6e6009c1936652f06
jakewhrlwnd <justjamescreative@outlook.com>
2026-01-08 16:24:03 -0500

    Condensed the implemenation to six lines, using built-ins and generator expressions

--- freecodecamp/daily_coding_challenges/sorted_array.py
+++ freecodecamp/daily_coding_challenges/sorted_array.py
@@ -8,30 +8,44 @@ If the given array is:
 * In descending order (highest to lowest), return "Descending".
 * Not sorted in ascending or descending order, return "Not sorted".
 """
-def is_sorted(arr):
-    expected_pattern = ""
-    for i in range(len(arr) - 1):
-        if expected_pattern == "":
-            if arr[i] > arr[i + 1]:
-                expected_pattern = "Descending"
-            elif arr[i] < arr[i + 1]:
-                expected_pattern = "Ascending"
-            else:
-                continue
-        else:
-            if expected_pattern == "Descending":
-                if arr[i] >= arr[i + 1]:
-                    continue
-                else:
-                    return "Not sorted"
-            elif expected_pattern == "Ascending":
-                if arr[i] <= arr[i + 1]:
-                    continue
-                else:
-                    return "Not sorted"
-            else:
-                return "Not sorted"
+# def is_sorted(arr):
+#     expected_pattern = ""
+#     for i in range(len(arr) - 1):
+#         if expected_pattern == "":
+#             if arr[i] > arr[i + 1]:
+#                 expected_pattern = "Descending"
+#             elif arr[i] < arr[i + 1]:
+#                 expected_pattern = "Ascending"
+#             else:
+#                 continue
+#         else:
+#             if expected_pattern == "Descending":
+#                 if arr[i] >= arr[i + 1]:
+#                     continue
+#                 else:
+#                     return "Not sorted"
+#             elif expected_pattern == "Ascending":
+#                 if arr[i] <= arr[i + 1]:
+#                     continue
+#                 else:
+#                     return "Not sorted"
+#             else:
+#                 return "Not sorted"
+#
+#     if expected_pattern == "":
+#         return "Not sorted"
+#     return expected_pattern
 
-    if expected_pattern == "":
+# More Pythonic
+# Created adjacent pairs with zip()
+# Checked the conditions across sequences with all()
+# Used generator expressions for verbosity
+# "Lazy evaluation over intermediate lists"
+
+def is_sorted(arr):
+    if all(a > b for a, b in zip(arr, arr[1:])):
+        return "Descending"
+    elif all(a < b for a, b in zip(arr, arr[1:])):
+        return "Ascending"
+    else:
         return "Not sorted"
-    return expected_pattern

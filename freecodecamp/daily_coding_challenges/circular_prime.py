06ae9fd010ab70a80d33cfecdf64341f21ae128a
jakewhrlwnd <justjamescreative@outlook.com>
2026-01-09 15:40:19 -0500

    Used all() to condense the code, add a helper function for the rotations

--- freecodecamp/daily_coding_challenges/circular_prime.py
+++ freecodecamp/daily_coding_challenges/circular_prime.py
@@ -6,21 +6,30 @@ its digits are themselves prime.
 *For example, 197 is a circular prime because all rotations
 of its digits: 197, 971, and 719, are prime numbers.
 """
+# def is_circular_prime(n):
+#     def is_prime(num):
+#         if num <= 1:
+#             return False
+#         for i in range(2, int(num ** 0.5) + 1):
+#             if num % i == 0:
+#                 return False
+#         return True
+#
+#     current = str(n)
+#     for j in range(len(str(n))):
+#         if not is_prime(int(current)):
+#             return False
+#         current = current[1:] + current[0]
+#     return True
+# More Pythonic
 def is_circular_prime(n):
     def is_prime(num):
         if num <= 1:
             return False
-        for i in range(2, int(num ** 0.5) + 1):
-            if num % i == 0:
-                return False
-        return True
+        return all(num % i != 0 for i in range(2, int(num**0.5) + 1))
 
-    current = str(n)
-    for j in range(len(str(n))):
-        if not is_prime(int(current)):
-            return False
-        current = current[1:] + current[0]
-    return True
+    def get_rotations(num_str):
+        return [num_str[i:] + num_str[:i] for i in range(len(num_str))]
 
-if __name__ == '__main__':
-    is_circular_prime(100025687741)
+    rotations = get_rotations(str(n))
+    return all(is_prime(int(rotation)) for rotation in rotations)

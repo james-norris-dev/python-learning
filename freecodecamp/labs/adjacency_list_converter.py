df1c8f2f214090576891bb823a01323ddd1fa8e5
jakewhrlwnd <justjamescreative@outlook.com>
2026-01-09 16:51:42 -0500

    Added the initial file setup

---
+++ freecodecamp/labs/adjacency_list_converter.py
@@ -0,0 +1,8 @@
+"""
+Build an Adjacency List to Matrix Converter
+* In this lab, you will build a function that converts an adjacency list
+representation of a graph into an adjacency matrix
+"""
+
+def adjacency_list_to_matrix():
+    pass

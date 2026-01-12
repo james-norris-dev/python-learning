eab7b5ff139fda029d7f8412caa0b08aa988227b
jakewhrlwnd <justjamescreative@outlook.com>
2026-01-12 15:34:00 -0500

    Updated the implementation for simplification

--- freecodecamp/daily_coding_challenges/plant_the_crop.py
+++ freecodecamp/daily_coding_challenges/plant_the_crop.py
@@ -1,21 +1,25 @@
 """
-Given an integer representing the size of your farm field, and "acres" or "hectares" representing the unit for the size
-of your farm field, and a type of crop, determine how many plants of that type you can fit in your field.
+Given an integer representing the size of your farm field,
+and "acres" or "hectares" representing the unit for the size
+of your farm field, and a type of crop, determine how many
+plants of that type you can fit in your field.
 """
+CONVERSION_FACTORS = {
+    "acres": 4046.86,
+    "hectares": 10000
+}
 
 def get_number_of_plants(field_size, unit, crop):
     crop_unit = unit.lower()
-    total_square_meters = 0
-    plant_count = 0
-    ACRE_SQUARE_METER = 4046.86
-    HECTARE_SQUARE_METER = 10000
+    crop_name = crop.lower()
 
-    if crop_unit != "acres" and crop_unit != "hectares":
+    if field_size <= 0:
+        raise ValueError("Field size must be a positive value")
+
+    if crop_unit not in CONVERSION_FACTORS:
         raise ValueError("Invalid crop unit: must be either 'acres' or 'hectares'")
-    elif crop_unit == "acres":
-        total_square_meters = field_size * ACRE_SQUARE_METER
-    elif crop_unit == "hectares":
-        total_square_meters = field_size * HECTARE_SQUARE_METER
+    else:
+        total_square_meters = field_size * CONVERSION_FACTORS[crop_unit]
 
     crop_spacing = {
         "corn": 1,
@@ -25,10 +29,9 @@ def get_number_of_plants(field_size, unit, crop):
         "lettuce": 0.2
     }
 
-    if crop in crop_spacing:
-        plant_count = int(total_square_meters / crop_spacing[crop])
+    if crop_name not in crop_spacing:
+        raise ValueError(f"Invalid crop: {crop}")
+    else:
+        plant_count = total_square_meters // crop_spacing[crop_name]
 
     return plant_count
-
-if __name__ == "__main__":
-    print(get_number_of_plants(2, "hectares", "lettuce"))

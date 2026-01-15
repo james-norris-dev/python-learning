5614cbb713c264bc5b39637094815004e8b7062b
jakewhrlwnd <justjamescreative@outlook.com>
2026-01-15 17:05:40 -0500

    Converted the loops to list comprehensions

--- freecodecamp/labs/adjacency_list_converter.py
+++ freecodecamp/labs/adjacency_list_converter.py
@@ -3,23 +3,42 @@ Build an Adjacency List to Matrix Converter
 * In this lab, you will build a function that converts an adjacency list
 representation of a graph into an adjacency matrix
 """
-def adjacency_list_to_matrix(graph):
-    num_nodes = len(graph)
-    matrix = []
+# def adjacency_list_to_matrix(graph):
+#     num_nodes = len(graph)
+#     matrix = []
+#
+#     for node in range(num_nodes):
+#         connections = graph[node]
+#         row = []
+#         for destination in range(num_nodes):
+#             if destination in connections:
+#                 row.append(1)
+#             else:
+#                 row.append(0)
+#         print(row)
+#         matrix.append(row)
+#     return matrix
 
-    for node in range(num_nodes):
-        connections = graph[node]
-        row = []
-        for destination in range(num_nodes):
-            if destination in connections:
-                row.append(1)
-            else:
-                row.append(0)
+# Converted the inner loop to list comprehension
+# def adjacency_list_to_matrix(graph):
+#     num_nodes = len(graph)
+#     matrix = []
+#
+#     for node in range(num_nodes):
+#         connections = graph[node]
+#         row = [1 if destination in connections else 0 for destination in range(num_nodes)]
+#         print(row)
+#         matrix.append(row)
+#     return matrix
+
+# More Pythonic - Converted the outer loop into a list comprehension
+def adjacency_list_to_matrix(graph: list):
+    matrix = [[1 if destination in graph[node] else 0 for destination in range(len(graph))] for node in range(len(graph))]
+    for row in matrix:
         print(row)
-        matrix.append(row)
-    return matrix
 
-if __name__ == "__main__":
-    print(adjacency_list_to_matrix({0: [1, 2], 1: [2], 2: [0, 3], 3: [2]}))
-    print(adjacency_list_to_matrix({0: [1], 1: [0]}))
-    print(adjacency_list_to_matrix({0: [], 1: [], 2: []}))
+    return matrix
+# if __name__ == "__main__":
+#     print(adjacency_list_to_matrix({0: [1, 2], 1: [2], 2: [0, 3], 3: [2]}))
+#     print(adjacency_list_to_matrix({0: [1], 1: [0]}))
+#     print(adjacency_list_to_matrix({0: [], 1: [], 2: []}))

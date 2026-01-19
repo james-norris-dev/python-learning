9eb6b608991fffe6205eb8fef54c54e13ef3d198
jakewhrlwnd <justjamescreative@outlook.com>
2026-01-19 16:46:39 -0500

    Passing attempt for the daily coding challenge

---
+++ freecodecamp/daily_coding_challenges/energy_consumption.py
@@ -0,0 +1,21 @@
+"""
+Energy Consumption
+
+Given the number of Calories burned during a workout,
+and the number of watt-hours used by your electronic devices during that workout,
+determine which one used more energy.
+"""
+CALORIE_IN_JOULES = 4184
+WATT_HOUR_IN_JOULES = 3600
+def compare_energy(calories_burned: int, watt_hours_used: int) -> str:
+    calories_burned_in_joules = calories_burned * CALORIE_IN_JOULES
+    watt_hours_in_joules = watt_hours_used * WATT_HOUR_IN_JOULES
+
+    if calories_burned_in_joules == watt_hours_in_joules:
+        result = "Equal"
+    elif calories_burned_in_joules > watt_hours_in_joules:
+        result = "Workout"
+    else:
+        result = "Devices"
+
+    return result
---
+++ freecodecamp/labs/depth_first_search_algorithm.py
@@ -0,0 +1,6 @@
+"""
+ Implement a graph traversal algorithm called depth-first search
+"""
+
+def dfs(matirx, node_label):
+    pass

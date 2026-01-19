07d44471de248edd585479eeaca63d9ea84e7b80
jakewhrlwnd <justjamescreative@outlook.com>
2026-01-19 16:52:39 -0500

    Condensed the if/elif/else to a conditional expression

--- freecodecamp/daily_coding_challenges/energy_consumption.py
+++ freecodecamp/daily_coding_challenges/energy_consumption.py
@@ -7,15 +7,25 @@ determine which one used more energy.
 """
 CALORIE_IN_JOULES = 4184
 WATT_HOUR_IN_JOULES = 3600
+# def compare_energy(calories_burned: int, watt_hours_used: int) -> str:
+#     calories_burned_in_joules = calories_burned * CALORIE_IN_JOULES
+#     watt_hours_in_joules = watt_hours_used * WATT_HOUR_IN_JOULES
+#
+#     if calories_burned_in_joules == watt_hours_in_joules:
+#         result = "Equal"
+#     elif calories_burned_in_joules > watt_hours_in_joules:
+#         result = "Workout"
+#     else:
+#         result = "Devices"
+#
+#     return result
+
+# More Pythonic
 def compare_energy(calories_burned: int, watt_hours_used: int) -> str:
-    calories_burned_in_joules = calories_burned * CALORIE_IN_JOULES
-    watt_hours_in_joules = watt_hours_used * WATT_HOUR_IN_JOULES
+    calories_joules = calories_burned * CALORIE_IN_JOULES
+    watt_joules = watt_hours_used * WATT_HOUR_IN_JOULES
 
-    if calories_burned_in_joules == watt_hours_in_joules:
-        result = "Equal"
-    elif calories_burned_in_joules > watt_hours_in_joules:
-        result = "Workout"
-    else:
-        result = "Devices"
-
-    return result
+    # Condensed down to a conditional expression, similar to Java's ternary operator
+    return ("Equal" if calories_joules == watt_joules
+            else "Workout" if calories_joules > watt_joules
+            else "Devices")

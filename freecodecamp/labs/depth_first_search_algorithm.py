ab9e0344379bd1ec76b02fb3349e6962fde7de31
jakewhrlwnd <justjamescreative@outlook.com>
2026-01-20 16:37:10 -0500

    Implemented a depth-first-search algorithm

--- freecodecamp/labs/depth_first_search_algorithm.py
+++ freecodecamp/labs/depth_first_search_algorithm.py
@@ -2,5 +2,24 @@
  Implement a graph traversal algorithm called depth-first search
 """
 
-def dfs(matirx, node_label):
-    pass
+def dfs(matrix, node_label):
+    stack = [node_label]
+    visited = []
+
+    while stack:
+        current_node = stack.pop()
+        if current_node in visited:
+            continue
+        visited.append(current_node)
+        for i, connected in enumerate(matrix[current_node]):
+            if connected == 1:
+                stack.append(i)
+
+    return visited
+
+if __name__ == "__main__":
+    print(dfs([[0, 1, 0, 0], [1, 0, 1, 0], [0, 1, 0, 1], [0, 0, 1, 0]], 1))
+    print(dfs([[0, 1, 0, 0], [1, 0, 1, 0], [0, 1, 0, 1], [0, 0, 1, 0]], 3))
+    print(dfs([[0, 1, 0, 0], [1, 0, 1, 0], [0, 1, 0, 0], [0, 0, 0, 0]], 3))
+    print(dfs([[0, 1, 0, 0], [1, 0, 0, 0], [0, 0, 0, 1], [0, 0, 1, 0]], 3))
+    print(dfs([[0, 1, 0, 0], [1, 0, 0, 0], [0, 0, 0, 1], [0, 0, 1, 0]], 0))
